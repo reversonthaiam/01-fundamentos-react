9ba2ccb871deb2fec714f355cbd1dc4b1463e0de
Reverson Thaiam <reverson.santos@frotabank.com.br>
2022-08-19 10:53:17 -0300

    post

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,83 @@ import styles from './App.module.css'
 import './global.css'
 import Post from './components/Post/post'
 
-function App() {
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/70376757?v=4',
+      name: 'Reverson Thaiam',
+      role: 'CTO @RevinCompany',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'paragraph', content: 'jane.design/doctorcare' },
+      { type: 'link', content: '#novoprojeto #nlw #rocketseat' },
+    ],
+    publishedAt: new Date('2022-02-03 20:08:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/70376757?v=4',
+      name: 'Diego Fernandes',
+      role: 'CTO @RocketSeat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'paragraph', content: 'jane.design/doctorcare' },
+      { type: 'link', content: '#novoprojeto #nlw #rocketseat' },
+    ],
+    publishedAt: new Date('2022-02-03 20:08:00'),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/70376757?v=4',
+      name: 'Pedro Joao',
+      role: 'CTO @RevinCompany',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'paragraph', content: 'jane.design/doctorcare' },
+      { type: 'link', content: '#novoprojeto #nlw #rocketseat' },
+    ],
+    publishedAt: new Date('2022-02-03 20:08:00'),
+  },
+]
 
+function App() {
   return (
     <div className="App">
-     <Header />
-    <div className={styles.wrapper}>
-      <Sidebar></Sidebar>
-      <main>
-        <Post></Post>
-        <Post></Post>
-      </main>
-    </div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar></Sidebar>
+        <main>
+          {posts.map((e) => (
+            <Post
+              key={e.id}
+              author={e.author}
+              content={e.content}
+              publishedAt={e.publishedAt}
+            />
+          ))}
+        </main>
+      </div>
     </div>
   )
 }
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,43 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import Avatar from '../Avatar/Avatar'
 import Comment from '../Comment/Comment'
 import styles from './Post.module.css'
 
-function Post() {
+function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([]);
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "dd 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    },
+  )
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  function handleCreateNewComment(e) {
+    event.preventDefault();
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/70376757?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Reverson Thaiam</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋 </p>
-
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-
-        <p>
-          👉 <a href="#"> jane.design/doctorcare </a>
-        </p>
-
-        <p>
-          <a href="#"> #novoprojeto #nlw #rocketseat</a>
-        </p>
+        {content.map((line) => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea placeholder="deixe um comentário" />
@@ -48,8 +65,6 @@ function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
         <Comment />
       </div>
     </article>
